6467d7a4deeca539ca906db822ad1fa54e3d8f02
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-14 19:51:19 +0900

    refactor: Use slash package to convert Windows paths

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -1,13 +1,18 @@
+import slash from 'slash';
+
 type CreateWorkspaceFileProps = {
   path: string;
 };
 
 export function createWorkspaceTemplate({ path }: CreateWorkspaceFileProps) {
+  // Windows backslash path (e.g. `C:\Users\xxx\yyy`) is not valid in vscode workspace json.
+  const slashedPath = slash(path);
+
   return `
 {
   "folders": [
     {
-      "path": "${path}"
+      "path": "${slashedPath}"
     }
   ]
 }`.trim();
