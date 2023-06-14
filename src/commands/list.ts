c127507222fd7326522a19dff961ef18dbfc16ec
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-14 18:54:39 +0900

    feat: add list subcommand

---
+++ src/commands/list.ts
@@ -0,0 +1,20 @@
+import { command } from 'cleye';
+import colors from 'picocolors';
+
+import { getWorkspaces } from '../operations/get-workspaces';
+
+export default command(
+  {
+    name: 'list',
+  },
+  async () => {
+    const workspaces = await getWorkspaces();
+    const msg = [
+      `${colors.cyan('All workspaces')} (total: ${workspaces.length})`,
+      ...workspaces.map(
+        (w) => `${colors.white(w.workspace)} ${colors.dim(w.path)}`
+      ),
+    ].join('\n');
+    console.log(msg);
+  }
+);
---
+++ test/operations/get-workspaces.test.ts
@@ -0,0 +1,43 @@
+import { existsSync } from 'node:fs';
+import { mkdir, rm } from 'node:fs/promises';
+import { basename, resolve } from 'node:path';
+
+import { afterEach, beforeEach, expect, test, vi } from 'vitest';
+
+import { createWorkspace } from '../../src/operations/create-workspace';
+import { getWorkspaces } from '../../src/operations/get-workspaces';
+
+const mocks = vi.hoisted(() => {
+  return {
+    testHomedir: () => resolve('test-tmp', basename(import.meta.url)),
+  };
+});
+
+beforeEach(async () => {
+  if (existsSync(mocks.testHomedir())) {
+    await rm(mocks.testHomedir(), { recursive: true, force: true });
+  }
+  await mkdir(mocks.testHomedir(), { recursive: true });
+
+  vi.mock('node:os', () => {
+    return {
+      homedir: () => mocks.testHomedir(),
+    };
+  });
+});
+
+afterEach(async () => {
+  vi.restoreAllMocks();
+});
+
+test('getWorkspaces', async () => {
+  expect((await getWorkspaces()).length).toBe(0);
+
+  await createWorkspace('dir-1');
+  const workspaces = await getWorkspaces();
+  expect(workspaces.length).toBe(1);
+
+  expect(workspaces.at(0)?.workspace).toBe(
+    resolve(mocks.testHomedir(), '.codew/workspaces/dir-1.code-workspace')
+  );
+});
