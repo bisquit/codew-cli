9fdf42e9a9b63e2eb42bf65cf8fdd32f73a8229e
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-06-14 18:19:22 +0900

    feat: add checkDir function for validating path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+import { log } from '@clack/prompts';
 import { cli } from 'cleye';
 
 import * as pkg from '../package.json';
+import { checkDir } from './operations/check-dir';
 import { createWorkspace } from './operations/create-workspace';
 import { getWorkspace } from './operations/get-workspace';
 import { openWorkspace } from './operations/open-workspace';
@@ -24,10 +26,16 @@ const argv = cli({
 
 const path = argv._.path;
 
-const workspace = await getWorkspace(path);
-if (workspace) {
-  await openWorkspace(workspace);
-} else {
-  const workspace = await createWorkspace(path);
-  await openWorkspace(workspace);
+try {
+  await checkDir(path);
+
+  const workspace = await getWorkspace(path);
+  if (workspace) {
+    await openWorkspace(workspace);
+  } else {
+    const workspace = await createWorkspace(path);
+    await openWorkspace(workspace);
+  }
+} catch (e) {
+  log.error(`${e}`);
 }
