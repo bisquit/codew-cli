41962362f7df30ae717cfb658e2e07c0240c709f
shuheikikuchi <qu1dpr09u0@gmail.com>
2023-09-04 17:20:13 +0900

    chore: modify preset

--- release.config.cjs
+++ release.config.cjs
@@ -3,4 +3,18 @@
 /** @type {import('semantic-release').Options} */
 module.exports = {
   branches: ['main', 'next', { name: 'beta', prerelease: true }],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+  ],
 };
